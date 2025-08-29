268b4324d0acb99d3677116f33cd925e55394098
mertcan <dikmenmertcan@gmail.com>
2025-08-29 14:21:58 +0300

    Api

---
+++ src/test/java/pojos/JPHPojo.java
@@ -0,0 +1,14 @@
+package pojos;
+
+public class JPHPojo {
+
+    /*
+    Request Body
+{
+    "title": "Ahmet",
+    "body": "Merhaba",
+    "userId": 10,
+    "id": 70
+}
+     */
+}
---
+++ src/test/java/tests/P20_PUT_POJOKullanimi.java
@@ -0,0 +1,34 @@
+package tests;
+
+import baseUrlKlasoru.BaseUrl_JPH;
+
+public class P20_PUT_POJOKullanimi extends BaseUrl_JPH {
+
+/*
+
+        https://jsonplaceholder.typicode.com/posts/70 url'ine
+asagidaki body'e sahip bir PUT request yolladigimizda
+donen response'in response body'sinin
+asagida verilen ile ayni oldugunu test ediniz
+
+Request Body
+{
+    "title": "Ahmet",
+    "body": "Merhaba",
+    "userId": 10,
+    "id": 70
+}
+
+Expected Data :
+{
+    "title": "Ahmet",
+    "body": "Merhaba",
+    "userId": 10,
+    "id": 70
+}
+
+ */
+
+
+
+}
