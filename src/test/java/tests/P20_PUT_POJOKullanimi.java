f6576d23ddebadf740dd69d01ff805417cac5868
mertcan <dikmenmertcan@gmail.com>
2025-08-29 14:49:00 +0300

    Api

--- src/test/java/tests/P20_PUT_POJOKullanimi.java
+++ src/test/java/tests/P20_PUT_POJOKullanimi.java
@@ -1,34 +1,58 @@
-package tests;
 
+package tests;
 import baseUrlKlasoru.BaseUrl_JPH;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+import pojos.JPHPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class P20_PUT_POJOKullanimi extends BaseUrl_JPH {
+    /*
 
-/*
-
-        https://jsonplaceholder.typicode.com/posts/70 url'ine
-asagidaki body'e sahip bir PUT request yolladigimizda
-donen response'in response body'sinin
+https://jsonplaceholder.typicode.com/posts/70 url’ine
+asagidaki body’e sahip bir PUT request yolladigimizda
+donen response’in response body’sinin
 asagida verilen ile ayni oldugunu test ediniz
 
 Request Body
 {
-    "title": "Ahmet",
-    "body": "Merhaba",
-    "userId": 10,
-    "id": 70
+    “title”: “Ahmet”,
+    “body”: “Merhaba”,
+    “userId”: 10,
+    “id”: 70
 }
 
 Expected Data :
 {
-    "title": "Ahmet",
-    "body": "Merhaba",
-    "userId": 10,
-    "id": 70
+    “title”: “Ahmet”,
+    “body”: “Merhaba”,
+    “userId”: 10,
+    “id”: 70
 }
 
  */
 
+    @Test
+    public void test01(){
+        specJPH.pathParams("pp1","posts","pp2","70");
+        JPHPojo reqPOJO=new JPHPojo("Ahmet","Merhaba",7,70);
+
+        JPHPojo expPOJO=new JPHPojo("Ahmet","Merhaba",7,70);
+
+        Response response=given().contentType(ContentType.JSON).spec(specJPH).when().body(reqPOJO).put("/{pp1}/{pp2}");
+
+        JPHPojo resPOJO=response.as(JPHPojo.class);
+
+        assertEquals(expPOJO.getTitle(),resPOJO.getTitle());
+        assertEquals(expPOJO.getBody(),resPOJO.getBody());
+        assertEquals(expPOJO.getUserId(),resPOJO.getUserId());
+        assertEquals(expPOJO.getId(),resPOJO.getId());
+
+    }
 
 
 }
---
+++ src/test/java/tests/P21_POST_POJOKullanimi.java
@@ -0,0 +1,74 @@
+
+package tests;
+
+import BaseUrl.RestFullBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+import pojos.Restfull_bookingdatesPojo;
+import pojos.Restfull_expPojo;
+import pojos.Restfull_reqPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class P21_POST_POJOKullanimi extends RestFullBaseUrl {
+    /*
+https://restful-booker.herokuapp.com/booking url’ine
+asagidaki body’ye sahip bir POST request gonderdigimizde
+donen response’un id haric asagidaki gibi oldugunu test edin.
+Request body
+       {
+           “firstname” : “Ahmet”,
+           “lastname” : “Bulut”,
+           “totalprice” : 500,
+           “depositpaid” : false,
+           “bookingdates” : {
+               “checkin” : “2021-06-01”,
+               “checkout” : “2021-06-10”
+           },
+           “additionalneeds” : “wi-fi”
+       }
+Response Body // expected data
+ {
+    “bookingid”: 24,
+    “booking”: {
+        “firstname”: “Ahmet”,
+        “lastname”: “Bulut”,
+        “totalprice”: 500,
+        “depositpaid”: false,
+        “bookingdates”: {
+            “checkin”: “2021-06-01",
+            “checkout”: “2021-06-10"
+        },
+        “additionalneeds”: “wi-fi”
+    }
+}
+
+     */
+
+    @Test
+    public void Test01(){
+
+        specJPH.pathParam("pp1","booking");
+        Restfull_bookingdatesPojo bookingdatesPojo=new Restfull_bookingdatesPojo("2025-09-01","2025-09-10");
+        Restfull_reqPojo reqPOJO=new Restfull_reqPojo("Murat","BABAYİĞİT",1000,true,bookingdatesPojo,"wi-fi");
+
+
+        Restfull_expPojo expPojo=new Restfull_expPojo(24,reqPOJO);
+
+        Response response =given().contentType(ContentType.JSON).spec(specJPH).when().body(reqPOJO).post("/{pp1}");
+
+        Restfull_expPojo resPOJO=response.as(Restfull_expPojo.class);
+
+        assertEquals(expPojo.getBooking().getFirstname(),resPOJO.getBooking().getFirstname());
+        assertEquals(expPojo.getBooking().getLastname(),resPOJO.getBooking().getLastname());
+        assertEquals(expPojo.getBooking().getTotalprice(),resPOJO.getBooking().getTotalprice());
+        assertEquals(expPojo.getBooking().isDepositpaid(),resPOJO.getBooking().isDepositpaid());
+        assertEquals(expPojo.getBooking().getBookingdates().getCheckin(),resPOJO.getBooking().getBookingdates().getCheckin());
+        assertEquals(expPojo.getBooking().getBookingdates().getCheckout(),resPOJO.getBooking().getBookingdates().getCheckout());
+
+    }
+
+}
